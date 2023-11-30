0c54147d9e279e86b0b27451ea756c6bed477126
Muhet <muhedarius96@gmail.com>
2023-11-30 17:03:02 +0200

    changing env value

--- src/services/Request.jsx
+++ src/services/Request.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-
-const baseUrl = process.env.REACT_APP_BASE_URL_LOCAL;
+const baseUrl = process.env.REACT_APP_BASE_URL;
 
 export const instance = axios.create({
   baseURL: baseUrl,
